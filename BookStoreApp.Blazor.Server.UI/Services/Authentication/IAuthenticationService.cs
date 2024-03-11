bfb9dd3e036ab32a6e5a3827e34b4cd8fd2b57fe
Christopher Williams <christopher.williams@iplicit.com>
2024-03-11 15:25:59 +0000

    Create blazor app

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> AuthenticateAsync(UserLoginDTO loginModel);
+        public Task Logout();
+
+    }
+}
